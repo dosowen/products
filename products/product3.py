95fc9ffa17f1e8126eebfe6587a9d0093d321a19
dosowen <s103394@cmes.tn.edu.tw>
2018-07-12 20:35:52 +0800

    third

--- products/product3.py
+++ products/product3.py
@@ -14,14 +14,16 @@ def print_product(products):
 	for p in products:
 		print(p[0],'要',p[1],'塊錢')
 
-def write_file(filename):
+def write_file(filename,products):
 	with open (filename, 'w') as f:
 		f.write('商品,價格\n')
 		for p2 in products:
 			f.write(p2[0] + ',' + p2[1] + '\n')
+		return products
 
 
-def read_file(filename,products):
+def read_file(filename):
+	k = []
 	import os
 	if os.path.isfile(filename):
 		print('yeah ,I found it!!!')
@@ -30,12 +32,12 @@ def read_file(filename,products):
 				if '商品,價格' in line:
 					continue
 				name,price = line.strip().split(',')
-				products.append([name, price])
-		print(products)
+				k.append([name, price])
+		print(k)
 	else:
 		print('I did not found it!!!!!')
 
-products=input_data()
+products = input_data()
 print_product(products)
-write_file('products.csv')
-read_file('products.csv',products)
+products = write_file('products.csv',products)
+read_file('products.csv')
